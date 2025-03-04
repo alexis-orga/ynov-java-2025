043364dc02477991abb2661395a7574b4ff5b139
ORGA Alexis <alexis.orga@ynov.com>
2025-03-04 14:25:25 +0100

    oui

---
+++ src/fr/ynov/java/easy/fr/ynov/java/easy/LoopsFor.java
@@ -0,0 +1,10 @@
+package fr.ynov.java.easy;
+
+public class LoopsFor {
+
+    public static void main(String[] args) {
+        for (int i = 1; i<11; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/fr/ynov/java/easy/fr/ynov/java/easy/LoopsWhile.java
@@ -0,0 +1,12 @@
+package fr.ynov.java.easy;
+
+public class LoopsWhile {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}
