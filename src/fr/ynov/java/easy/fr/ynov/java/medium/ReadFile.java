63c9e3f595b5899628056f63d491b7d67f3efeb3
ORGA Alexis <alexis.orga@ynov.com>
2025-03-13 16:59:39 +0100

    reading files w/ folder

--- src/fr/ynov/java/easy/fr/ynov/java/medium/ReadFile.java
+++ src/fr/ynov/java/easy/fr/ynov/java/medium/ReadFile.java
@@ -1,22 +1,24 @@
 package fr.ynov.java.medium;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try {
-            File myObj = new File("ynov-java-2025\\src\\fr\\ynov\\java\\easy\\fr\\ynov\\java\\medium\\trucalire.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+        File myObj = new File("ynov-java-2025\\src\\fr\\ynov\\java\\easy\\fr\\ynov\\java\\medium\\trucalire");
+        File[] files = myObj.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                try (Scanner scanner = new Scanner(file)) {
+                    while (scanner.hasNextLine()) {
+                        System.out.println(scanner.nextLine());
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error reading file: " + file.getName());
+                    e.printStackTrace();
+                }
             }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred");
-            e.printStackTrace();
         }
     }
 }
