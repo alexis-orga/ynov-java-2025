432bb59004c97848c40afe3af540884408bed5cf
ORGA Alexis <alexis.orga@ynov.com>
2025-03-13 13:51:43 +0100

    person.java w/ age

--- src/fr/ynov/java/easy/fr/ynov/java/medium/Person.java
+++ src/fr/ynov/java/easy/fr/ynov/java/medium/Person.java
@@ -4,24 +4,40 @@ import java.util.Scanner;
 
 public class Person {
 
-    String name;
-    Integer age;
-    String gender;
-    float height;
-    float weight;
-    Enum nationality;
+    private String name;
+    private Integer age;
+    private String gender;
+    private float height;
+    private float weight;
+    private Nationality nationality;
 
-    public Person {
-        name = new name();
-        age = new age();
-        gender = new gender();
-        height = new height();
-        weight = new weight();
-        nationality = new nationality();
+    public Person (String name, Integer age, String gender, float height, float weight, Nationality nationality) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+        this.nationality = nationality;
     }
 
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age " + age);
+        System.out.println("Gender: " + gender);
+        System.out.println("Height: " + height + "m");
+        System.out.println("Weight: " + weight + "kg");
+        System.out.println("Nationality: " + nationality);
+    }
     public static void main(String[] args) {
-        Person myObjPerson = new Person();
-        System.out.println(myObjPerson);
+        Person person = new Person("John", 12, "Homme", (float) 1.76, 80, Nationality.FRENCH);
+        person.displayInfo();
     }
+}
+
+enum Nationality {
+    FRENCH,
+    AMERICAN,
+    BRITISH,
+    GERMAN,
+    SPANISH
 }
